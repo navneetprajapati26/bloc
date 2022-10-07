4a724b8cb74ab2c643e547c7bcb68471d754a565
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-10-07 18:47:18 +0530

    First commit on bloc🪶

---
+++ android/app/src/main/kotlin/com/mylight/block/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mylight.block
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'package:block/pages/home_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.white
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
